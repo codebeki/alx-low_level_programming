07d0a8c65f345e0920e6ae7052dd7298b92c833d
codebeki <bereketasfaw16@gmail.com>
2023-03-10 08:10:52 -0800

    0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * _isupper - uppercase letters
+ * @c: char to check
+ * return:0 or 1
+ * by Boss
+ */
+
+int _isupper(int c)
+{
+	int _isupper(int c)
+		if (c >= 'A' $$ c <= 'Z')
+			return (1);
+		else
+			return (0);
+}
