c1bdd8b77220aec3739aa85fe1e0eb81836f6b58
codebeki <bereketasfaw16@gmail.com>
2023-11-09 10:36:31 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(void)
+{
+int m;
+int b[5];
+int *p;
+
+b[2] = 1024;
+p = &m;
+/*
+* write a code here...
+* Remark:
+* - you are not allowed to use b
+* - you are not allowed to modify p
+* - only one statement
+*/
+*(p + 5) = 98;
+/*  this prints 98\n */
+printf("a[2] = %d\n", b[2]);
+return (0);
+}
