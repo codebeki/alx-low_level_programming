d6da04b5a4d4907c77e0772ad9818afa81984b66
codebeki <bereketasfaw16@gmail.com>
2023-11-09 12:01:50 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,4 @@
-# include <stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -9,15 +9,15 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* write a code here...
-* Remark:
-* - you are not allowed to use b
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statement
-*   ou are not allowed to code anything else than the line of expected line of code at the expected line
+* - you are not allowed to code anything else than this line of code
 */
-*(p + 5) = 98;
-/*  this prints 98\n */
-printf("a[2] = %d\n", a[2]);
+p[5] = 98;
+/* ...so that this prints 98\n */
+pintf("a[2] = %d\n", a[2]);
 return (0);
 }
