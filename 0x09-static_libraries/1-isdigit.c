c9102a560b05f05b7ea97d689ae0a3bd99c58b0c
codebeki <bereketasfaw16@gmail.com>
2023-11-17 05:41:49 -0800

    1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -3,7 +3,8 @@
 /**
  * _isdigit - checks if parameter is a number between 0 to 9.
  * @c: input number.
- * Return: 1 if is a number (0 to 9), 0 in other case.
+ * Return: 1 if is a number (0 to 9), 0 in other case
+ * by beki
  */
 int _isdigit(int c)
 {
