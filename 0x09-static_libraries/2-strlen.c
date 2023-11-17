6196aab11ef2e2f95af435ef117ee31db2b22064
codebeki <bereketasfaw16@gmail.com>
2023-11-17 06:24:15 -0800

    2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen - function that returns the length of a string
- *
- * @s: parameter defined in main
- *
- * Return: length of string
+ * _strlen - returns the length of a string.
+ * @s: input stringeturn.
+ * Return: length of a string.
  * by beki
  */
-
 int _strlen(char *s)
 {
-	int i = 0;
+	int count = 0;
 
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-	return (i);
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
 }
