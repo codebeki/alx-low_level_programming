a07661f3cce9df74f9ad7e7bea21f86bbff3f3e5
codebeki <bereketasfaw16@gmail.com>
2023-11-17 05:34:44 -0800

    1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9)
- *
- * @c: parameter hard-coded in main
- *
- * Return: 1 or 0
+ * _isdigit - checks if parameter is a number between 0 to 9.
+ * @c: input number.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
  */
-
 int _isdigit(int c)
 {
-	int i;
-
-	if (c >= '0' && c <= '9')
+	if (c >= 48 && c <= 57)
 	{
-		i = 1;
+		return (1);
 	}
 	else
 	{
-		i = 0;
+		return (0);
 	}
-	return (i);
 }
