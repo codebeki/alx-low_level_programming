7804277edbefc2365e2d9f0fe92c3fec61e471a5
codebeki <bereketasfaw16@gmail.com>
2023-11-17 08:55:50 -0800

    5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -2,53 +2,27 @@
 #include <stdio.h>
 
 /**
- * _strstr - function that locates a substring
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
  *
- * @haystack: parameter defined in main, pointer to memory (string)
- * @needle: parameter defined in main, characters to be compared with s string
- *
- * Return: a pointer to the beginning of the located substring
- * or NULL if the substring is not found
+ * Return: a pointer to the byte in s that matches one of the
+ * bytes in accept, or NULL if no such byte is found.
+ * by beki
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int size = 0;
-	char *ptr = haystack;
-	char *tmp1 = needle;
 	int i, j;
 
-	while (*needle++)
-		size++;
-	needle = tmp1;
-
-	while (*haystack)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		needle = tmp1;
-		i = 0;
-		while (needle < tmp1 + size)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (*haystack == *needle)
-			{
-				i++;
-			}
-			ptr = haystack;
-			needle++;
-			haystack++;
-			for (j = 1; j < size; j++)
-			{
-				if (*haystack == *needle)
-				{
-					i++;
-				}
-				needle++;
-				haystack++;
-			}
+			if (haystack[i + j] != needle[j])
+				break;
 		}
-		if (i == size)
-			return (ptr);
-		haystack = ptr;
-		haystack++;
+		if (!needle[j])
+			return (&haystack[i]);
 	}
 	return (NULL);
 }
