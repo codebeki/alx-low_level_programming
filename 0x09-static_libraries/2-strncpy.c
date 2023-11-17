f32d4267068727aefe4bffaee3cff1836b46cbd3
codebeki <bereketasfaw16@gmail.com>
2023-11-17 06:29:00 -0800

    2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,51 +1,21 @@
 #include "main.h"
-
-#include <stdio.h>
-
 /**
- * _strncpy - function that concatenates two strings
- *
- * @dest: parameter defined in main
- * @src: parameter defined in main
- * @n: parameter defined in main
- *
- * Return: memory address of function (string)
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src:the  source.
+ * @n:the  amount of bytes from src.
+ * Return: the pointer to dest.
+ * by beki
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
-	char *tmp1 = dest;
-	char *tmp2 = src;
-
-	while (src < tmp2 + n && *src != '\0')
-	{
-		*dest = *src;
-		src++;
-		dest++;
-	}
-	while (src < tmp2 + n)
-	{
-		*dest = '\0';
-		dest++;
-		src++;
-	}
-	dest = tmp1;
-	src = tmp2;
-
-/*
- *	int i;
- *	char *tmp = dest;
- *
- *	for (i = 0; i < n && *src != '\0'; i++)
- *		*dest++ = *src++;
- *
- *	for ( ; i < n; i++)
- *		*dest++ = '\0';
- *
- *	dest = tmp;
- */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
-
