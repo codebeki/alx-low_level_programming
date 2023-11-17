93712b633a7c9ea7adf81828ebcc265c78c8cf5f
codebeki <bereketasfaw16@gmail.com>
2023-11-17 08:31:31 -0800

    3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,39 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strspn - function that gets the length of a prefix substring
- *
- * @s: parameter defined in main, pointer to memory (string)
- * @accept: parameter defined in main, characters to be compared with s string
- * Return: integer, number of bytes common to the two strings
- * by Boss
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int size = 0;
-	unsigned int bytes = 0;
-	char *tmp = accept;
-	int i;
+	unsigned int i, j, bool;
 
-	while (*accept++)
-		size++;
-	accept = tmp;
-
-	while (*s)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		accept = tmp;
-		i = 0;
-		while (accept < tmp + size)
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*s == *accept)
-				bytes++, i++;
-			accept++;
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
 		}
-		if (i == 0)
+		if (bool == 1)
 			break;
-		s++;
 	}
-	return (bytes);
+	return (i);
 }
