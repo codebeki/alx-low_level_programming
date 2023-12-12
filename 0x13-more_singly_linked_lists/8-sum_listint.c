37b6f06065f2ec46cf7e70c8a9fc49fbe41b120b
root <bereketasfaw16@gmail.com>
2023-12-11 22:04:59 -0800

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,9 +1,9 @@
-#include "stdio,h"
+#include "stdio.h"
 #include "lists.h"
 
 /**
- * sum_listint - sum up all the value in each element of a listint_t
- * @head: pointer to the head of a list.
+ * sum_listint -  the sum up all the value in each element of a listint_t
+ * @head:  the pointer to the head of a list.
  *
  * Return: Sum (INT) else 0 if empty.
  * by beki
