ce3b505dc844c5677d1935e4ad751840302847b2
root <bereketasfaw16@gmail.com>
2023-12-11 21:53:51 -0800

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,18 +1,16 @@
+#include "stdio.h"
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
- * @head: listint_t list to be freed
- * by Boss
+ * free_listint - frees all the node in a list.
+ * @head: the pointer to a list.
+ * by beki
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
-
-	while (head)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head);
 }
+
