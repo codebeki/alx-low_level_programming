5cdb749c10a22c19d10481dcf8bfe0c66b583fe8
codebeki <bereketasfaw16@gmail.com>
2023-12-24 00:59:22 -0800

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * flip_bits - counts the number of bits to change
+ * flip_bits - it  counts the number of bits to change
  * to get from one number to another
- * @n: first number
- * @m: second number
- * Return: number of bits to change
+ * @n: the  first number
+ * @m:the  second number
+ * Return: the  number of bits to change
  * by Boss
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
